c29658be04ad277d94f26439c4c6040eaea0c7ce
Yifan Ren <51114968+fanrenaz@users.noreply.github.com>
2020-04-16 20:41:33 -0400

    Add files via upload

--- ReviewManager.py
+++ ReviewManager.py
@@ -3,11 +3,8 @@
 
 import pandas as pd
 import nltk
-import string
-import os
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
-import re
 from math import log
 from collections import Counter
 import warnings
@@ -23,23 +20,23 @@ class ReviewManager:
     def read_dataframe(self,df):
         self.data = df
     
-    def tokenize(text):
+    def tokenize(self, text):
         tokens = nltk.word_tokenize(text.lower())
-        nonstop = [i for i in tokens if i not in stopwords_]
+        nonstop = [i for i in tokens if i not in self.stopwords_]
         stems = [PorterStemmer().stem(x) for x in nonstop if x.isalpha() is True]
         return stems
 
     def tokenize_review(self):
-        stopwords_ = stopwords.words('english')
-        stopwords_.extend(['!',',','.','?','-s','-ly','</s>',"'s"])
-        self.data["tokens"] = self.data["review"].map(tokenize)
+        self.stopwords_ = stopwords.words('english')
+        self.stopwords_.extend(['!',',','.','?','-s','-ly','</s>',"'s"])
+        self.data["tokens"] = self.data["review"].map(self.tokenize)
     
     def calculate_document_frequency(self,inverse=True):
         warnings.warn("Note! Long Running Time")
         tokens_list = self.data["tokens"].to_list()[0].copy()
         for i in self.data["tokens"].to_list()[1:]:
             tokens_list.extend(i)
-        tokens_list = list(set(tokens))
+        tokens_list = list(set(tokens_list))
         df_dict = dict(zip(tokens_list,[0 for i in range(len(tokens_list))]))
         for sent in self.data["tokens"].to_list():
             for token in [i for i in tokens_list if i in sent]:
@@ -50,13 +47,13 @@ class ReviewManager:
         return idf_dict
     
     def calculate_tfidf(self, sentence):
-        tfidf_dict = {k : v * calculate_document_frequency()[k] 
+        tfidf_dict = {k : v * self.idf_dict[k] 
                       for k, v in dict(Counter(sentence)).items() 
-                      if k in calculate_document_frequency()}
+                      if k in self.idf_dict}
         return tfidf_dict
     
-    def get_keywords_sentence(tokens, threhold = 5, low_bound = 3):
-        tfidf_dict = calculate_tfidf(tokens)
+    def get_keywords_sentence(self, tokens, threhold = 5, low_bound = 3):
+        tfidf_dict = self.calculate_tfidf(tokens)
         keywords = [token for token in tfidf_dict.keys() if tfidf_dict[token] > threhold]
         while len(keywords) == 0:
             if threhold == low_bound: break
@@ -65,7 +62,8 @@ class ReviewManager:
         return keywords
     
     def get_keywords_total(self, verbose = False):
-        self.data["keywords"] = self.data["tokens"].map(get_keywords)
+        self.idf_dict = self.calculate_document_frequency()
+        self.data["keywords"] = self.data["tokens"].map(self.get_keywords_sentence)
         keyword_df = pd.DataFrame(self.data.set_index("recipe_id")["keywords"].groupby("recipe_id").sum())
         if verbose == True: return keyword_df
         keywords_distinct = keyword_df["keywords"].to_list()[0].copy()
@@ -74,11 +72,9 @@ class ReviewManager:
         return list(set(keywords_distinct))
     
     def fast_keywords(self, source, source_type = "csv"):
-        warnings.warn("Note! Long Running Time")
         if source_type == "csv":
             self.read_csv(source)
         if source_type == "dataframe":
             self.read_dataframe(source)
         self.tokenize_review()
         return self.get_keywords_total()
-
