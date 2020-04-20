38bc06fa0bafbf5ae00efb20f5e17226a5d3c67b
Yifan Ren <51114968+fanrenaz@users.noreply.github.com>
2020-04-20 11:55:31 -0400

    Add files via upload

--- recommender.py
+++ recommender.py
@@ -10,8 +10,8 @@ class Recommender:
         self.transformer_review = Review2Vec()
         self.transformer_recipe = Recipe2Vec()
     
-    def fit_transform(self, review_file, recipe_file):
-        self.transformer_review.fit_raw_data(review_file,'csv')
+    def fit_transform(self, review_file, recipe_file, review_type):
+        self.transformer_review.fit_raw_data(review_file,review_type)
         self.transformer_recipe.fit_raw_data(recipe_file)
         self.transformer_review.transform()
         self.transformer_recipe.transform_name()
@@ -19,15 +19,11 @@ class Recommender:
         self.transformer_recipe.transform_ingre()
     
     def generator(self, recipe_id):
-        recipe_df = self.transformer_review.calculate_distance(recipe_id)
-        name_df = self.transformer_recipe.calculate_name_distance(recipe_id)
-        tag_df = self.transformer_recipe.calculate_tag_distance(recipe_id)
-        ingre_df = self.transformer_recipe.calculate_ingre_distance(recipe_id)
-        for df in [recipe_df, name_df, tag_df, ingre_df]:
-            try:
-                ranking_df = recipe_df.merge(df, left_index=True, right_index=True)
-            except:
-                ranking_df = df
+        recipe_df = transformer_review.calculate_distance(recipe_id)
+        name_df = transformer_recipe.calculate_name_distance(recipe_id)
+        tag_df = transformer_recipe.calculate_tag_distance(recipe_id)
+        ingre_df = transformer_recipe.calculate_ingre_distance(recipe_id)
+        ranking_df = pd.concat([recipe_df, name_df, tag_df, ingre_df], join="outer", axis=1)
         ranking_df["score"] = ranking_df["keywords"]*ranking_df["recipe_name"]*ranking_df["recipe_tag"]*ranking_df["recipe_ingre"]
         ranking_df.sort_values("score",inplace = True)
         for id_ in ranking_df.index:
